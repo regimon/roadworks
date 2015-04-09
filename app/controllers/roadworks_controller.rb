0ba572aaa26d10104dc21164f8e892843a5a01cb
regimon <james_haverman@hotmail.com>
2015-04-09 16:05:28 +0100

    search added

--- app/controllers/roadworks_controller.rb
+++ app/controllers/roadworks_controller.rb
@@ -60,6 +60,10 @@ class RoadworksController < ApplicationController
       format.json { head :no_content }
     end
   end
+  
+  def search
+    @roadworks = Roadwork.search(params[:key])
+  end  
 
   private
     # Use callbacks to share common setup or constraints between actions.
--- app/models/roadwork.rb
+++ app/models/roadwork.rb
@@ -1,2 +1,11 @@
 class Roadwork < ActiveRecord::Base
+
+  def self.search(search)
+    if search
+      where('description LIKE ?', "%#{search}%")
+    else
+      all
+    end
+  end
+
 end
