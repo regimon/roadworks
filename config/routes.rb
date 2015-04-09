fa097265dde4921ecad2f36ac14a60a484554e2a
regimon <james_haverman@hotmail.com>
2015-04-09 14:41:11 +0100

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :roadworks
   resources :maps
+  
+  root 'roadworks#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
