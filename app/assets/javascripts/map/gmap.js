6e670a3b8e4fe91d3d73cb579389c62b895a3254
regimon <james_haverman@hotmail.com>
2015-04-11 21:07:19 +0100

    added info windows

--- app/assets/javascripts/map/gmap.js
+++ app/assets/javascripts/map/gmap.js
@@ -1,6 +1,7 @@
 
 function initialize() {
-
+	
+  rwMarkers = [];
   
   var mapOptions = {
     zoom: 12,
@@ -20,18 +21,56 @@ function initialize() {
 			url: '/ajax/getrws',
 			dataType: 'json'
 		}).success(function(rws){	
-			console.log(rws);
-			// Loops over rws, adding them to the map
 		    rws.forEach(function(rw){
-				console.log(rw);
-				// Creates marker for current rw
 				rwMarker = new google.maps.Marker({
 					position: new google.maps.LatLng(parseFloat(rw.latitude),parseFloat(rw.longitude)),
 					map	: map,
-					title	: "hello",
+					title	: rw.description,
 					icon	: '/images/closedroad.png'
-					}
-				)
+					})
+					
+					info = $('<div/>',{
+					'class'	: 'rw-info-window',
+					'html'	: [
+						$('<h5/>',{
+							'html'	: rw.description
+						}),
+						$('<div/>',{
+							'class'	: 'address-wrap',
+							'html'	: [
+							    $('<p/>',{
+									'html'	: '<b>Starts on:</b>'
+								}),
+								$('<p/>',{
+									'html'	: rw.startdate
+								}),
+								$('<p/>',{
+									'html'	: '<b>Ends at:</b>'
+								}),
+								$('<p/>',{
+									'html'	: rw.enddate
+								})
+							]
+						})
+					]
+				});
+				 // adds info window attributes to the rwMarkers 
+				rwMarker.sInfoWindow = new google.maps.InfoWindow({
+					content	: info.prop('outerHTML')
+				});
+				
+				// creates array of these markers
+				rwMarkers.push(rwMarker);
+				
+				// opens info window on click event (cycles through markers)
+				google.maps.event.addListener(rwMarker, 'click', function() {
+					rwMarkers.forEach(function(rwMarker){
+						rwMarker.sInfoWindow.close();
+					});
+					
+					this.sInfoWindow.open(map,this);
+				});
+				
 			})
 		 });
 
@@ -46,91 +85,3 @@ function loadScript() {
 }
 
 window.onload = loadScript;
-	
-	// Loads all listed Gyms from server
-/*	this.loadrws = function() {
-		$.ajax({
-			type: "POST",
-			url: '/ajax/getrws',
-			dataType: 'json'
-		}).success(function(rws){	
-			// Loops over rws, adding them to the map
-			rws.forEach(function(rw){
-				// Creates marker for current rw
-				rwMarker = new google.maps.Marker({
-					position: new google.maps.LatLng(rw.latitude, rw.longitude),
-					map	: map,
-					title	: rw.name,
-					icon	: '/images/closedroad.png'
-				});
-				
-				// Info window HTML
-				/*info = $('<div/>',{
-					'class'	: 'gym-info-window',
-					'html'	: [
-						$('<h5/>',{
-							'html'	: gym.name
-						}),
-						$('<div/>',{
-							'class'	: 'address-wrap',
-							'html'	: [
-								$('<p/>',{
-									'html'	: gym.address
-								}),
-								$('<p/>',{
-									'html'	: gym.postcode
-								})
-							]
-						})
-					]
-				});
-				
-				// Adds Gym telephone number, if one exists
-				if ( typeof gym.phone_number === 'string' ) {
-					info.append($('<div/>',{
-						'class'	: 'telephone-wrap',
-						'html'	: $('<p/>', {
-							'html'	 : gym.phone_number
-						})
-					}));
-				}
-				
-				// Adds contact email if one exists
-				if ( typeof gym.email === 'string' ) {
-					info.append($('<div/>',{
-						'class'	: 'email-wrap',
-						'html'	: $('<a/>',{
-							'href'	: 'mailto:' + gym.email,
-							'html'	: $('<div/>',{
-								'class'	: 'glyphicon glyphicon-envelope'
-							})
-						})
-					}));
-				}
-				
-				// Creates marker's info window and makes it a property of the marker
-				gymMarker.sInfoWindow = new google.maps.InfoWindow({
-					content	: info.prop('outerHTML')
-				});
-				
-				// Adds marker to centray array
-				gymMarkers.push(gymMarker);
-				
-				// Hides all open info windows then opens info winodw of clicked marker
-				google.maps.event.addListener(gymMarker, 'click', function() {
-					gymMarkers.forEach(function(gymMarker){
-						gymMarker.sInfoWindow.close();
-					});
-					
-					this.sInfoWindow.open(map,this);
-				});*/
-
-		//	});
-		//}); 
-	//}
-	
-	// Loads map and Gyms on class construct
-	//initialize();
-	//this.loadMap();
-	//this.loadrws();
-
